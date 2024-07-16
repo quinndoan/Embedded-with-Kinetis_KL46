ad790a6ea572c2f192eb0992f818dacc0513af4f
Đoàn Thị Thu Quyên <thuquyentntl@gmail.com>
2024-07-16 18:49:47 +0700

    bản chạy chính xác

---
+++ MOCK/Final1.c
@@ -0,0 +1,418 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "MKL46Z4.h"
+
+// Định nghĩa các hằng số và kiểu dữ liệu
+#define BUFFER_SIZE 256
+#define QUEUE_SIZE 15
+#define HEX_INVALID 255
+#define SREC_ERROR 0
+#define SUCCESS 1
+
+volatile uint8_t rxBuffer[BUFFER_SIZE];
+volatile uint8_t rxIndex = 0;
+volatile bool newData = false;
+
+typedef enum {
+    SREC_TRUE = 0x0,
+    CHECK_SUM_ERROR = 0x1,
+    SYNTAX_ERROR = 0x2,
+    BYTE_COUNT_ERROR = 0x3
+} SrecStatus_t;
+
+typedef struct Srec {
+    uint8_t recordType;
+    uint8_t sizeOfAddress;
+    uint32_t ByteCount;
+    uint32_t sizeOfData;
+    uint32_t checkSum;
+    uint32_t numberAddress; // dùng cho TH address 4 bytes, chuyển sẵn Address sang từ String sang Hex
+    char *Address;
+    char *Data;
+    uint8_t checksum;
+} Srec;
+
+typedef struct {
+    char data[BUFFER_SIZE];
+    uint16_t length;
+} QueueItem;
+
+typedef struct {
+    QueueItem items[QUEUE_SIZE];
+    uint8_t head;
+    uint8_t tail;
+    uint8_t count;
+} Queue;
+
+Queue srecQueue = {{0}, 0, 0, 0};
+
+void Program_LongWord_Command(uint32_t Address, uint32_t Data) {
+    //Wait previous command to finished
+    while ((FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK) == 0);
+
+    //Check Error and Protection flags
+    if (((FTFA->FSTAT & FTFA_FSTAT_ACCERR_MASK) & FTFA_FSTAT_FPVIOL_MASK) != 0) {
+        FTFA->FSTAT = 0x30;
+    }
+
+    //Command
+    FTFA->FCCOB0 = 0x06;
+
+    //Address
+    FTFA->FCCOB3 = (uint8_t)(Address & 0xFF);
+    FTFA->FCCOB2 = (uint8_t)((Address & 0xFF00) >> 8);
+    FTFA->FCCOB1 = (uint8_t)((Address & 0xFF0000) >> 16);
+
+    //Data
+    uint32_t *ptr = (uint32_t *)0x40020008;
+    *ptr = Data;
+
+    //Clear CCIF
+    FTFA->FSTAT = 0x80;
+}
+
+void Erase_Flash_Sector(uint32_t Address) {
+    //Wait previous command to finished
+    while ((FTFA->FSTAT & FTFA_FSTAT_CCIF_MASK) == 0);
+
+    //Check Error and Protection flags
+    if (((FTFA->FSTAT & FTFA_FSTAT_ACCERR_MASK) & FTFA_FSTAT_FPVIOL_MASK) != 0) {
+        FTFA->FSTAT = 0x30;
+    }
+
+    //Command
+    FTFA->FCCOB0 = 0x09;
+
+    //Address
+    FTFA->FCCOB3 = (uint8_t)(Address & 0xFF);
+    FTFA->FCCOB2 = (uint8_t)((Address & 0xFF00) >> 8);
+    FTFA->FCCOB1 = (uint8_t)((Address & 0xFF0000) >> 16);
+
+    //Clear CCIF
+    FTFA->FSTAT = 0x80;
+}
+
+void UART_Initialize() {
+	// Enable clock for UART0 and Port A
+		    SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;
+		    SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;
+
+		    // Configure PTA1 as UART0_RX and PTA2 as UART0_TX
+		    PORTA->PCR[1] = PORT_PCR_MUX(2U);  // Set PTA1 to UART0_RX
+		    PORTA->PCR[2] = PORT_PCR_MUX(2U);  // Set PTA2 to UART0_TX
+
+		    // Enable pull-up resistors on PTA1 and PTA2
+		    PORTA->PCR[1] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+		    PORTA->PCR[2] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+
+		    // Disable UART0 transmitter and receiver before configuration
+		    UART0->C2 &= ~(UART0_C2_TE_MASK | UART0_C2_RE_MASK);
+
+		    // Configure MCGIRCLK as UART0 clock source
+		    MCG->C1 |= MCG_C1_IRCLKEN_MASK; // Enable MCGIRCLK
+		    MCG->C2 |= MCG_C2_IRCS_MASK;    // Select fast internal reference clock
+		    MCG->SC &= ~MCG_SC_FCRDIV_MASK; // Set FCRDIV to 0 (divide by 1)
+
+		    // Select MCGIRCLK as UART0 clock source
+		    SIM->SOPT2 |= SIM_SOPT2_UART0SRC(3U);
+
+		    // Set baud rate (assuming a baud rate of 9600)
+		    uint16_t sbr = 26; // SBR value for 9600 baud with 4 MHz clock
+		    UART0->BDH = (sbr >> 8) & UART0_BDH_SBR_MASK; // dich 8 bit de chi lay ra phan cao nhat
+		    UART0->BDL = sbr & UART0_BDL_SBR_MASK;
+
+		    // Configure oversampling ratio to 16
+		    UART0->C4 = (UART0->C4 & ~UART0_C4_OSR_MASK) | UART0_C4_OSR(15);
+
+		    // Enable both edge sampling
+		    UART0->C5 |= UART0_C5_BOTHEDGE_MASK;
+
+		    // Configure 8-bit data, no parity, 1 stop bit
+		    UART0->C1 &= ~(UART0_C1_M_MASK | UART0_C1_PE_MASK);
+		    UART0->BDH &= ~UART0_BDH_SBNS_MASK;
+
+		    // Enable UART0 transmitter and receiver
+		    UART0->C2 |= UART0_C2_TE_MASK | UART0_C2_RE_MASK;
+
+		    // Enable UART0 receive interrupt
+		    UART0->C2 |= UART0_C2_RIE_MASK;
+
+		    // Enable UART0 interrupt in NVIC
+		    NVIC_EnableIRQ(UART0_IRQn);
+}
+
+void UART_SendChar(char c) {
+    // Wait for transmit data register empty flag
+    while (!(UART0->S1 & UART0_S1_TDRE_MASK));
+    // Send character
+    UART0->D = c;
+    // Wait for transmission complete flag
+    while (!(UART0->S1 & UART0_S1_TC_MASK));
+}
+
+void UART_TransmitString(uint8_t *ptr, uint8_t length) {
+    for (uint8_t i = 0; i < length; i++) {
+        UART_SendChar(ptr[i]);
+    }
+}
+
+void Queue_Enqueue(Queue *q, const char *data, uint16_t length) {
+    if (q->count < QUEUE_SIZE) {
+        q->items[q->head].length = length;
+        memcpy(q->items[q->head].data, data, length);
+        q->items[q->head].data[length] = '\0'; // Null terminate for safety
+        q->head = (q->head + 1) % QUEUE_SIZE;
+        q->count++;
+    }
+}
+
+bool Queue_Dequeue(Queue *q, QueueItem *item) {
+    if (q->count == 0) {
+        return false;
+    } else {
+        *item = q->items[q->tail];
+        q->tail = (q->tail + 1) % QUEUE_SIZE;
+        q->count--;
+        return true;
+    }
+}
+
+uint8_t char_to_hex(uint8_t c) {
+    if ('0' <= c && c <= '9') {
+        return c - '0';
+    } else if ('A' <= c && c <= 'F') {
+        return c - 'A' + 10;
+    } else {
+        return HEX_INVALID;
+    }
+}
+
+uint8_t StrtoHex(const char *str, uint8_t len, uint32_t *pDecimal) {
+    *pDecimal = 0;
+    for (uint32_t idx = 0; idx < len; idx++) {
+        uint8_t hex = char_to_hex(str[idx]);
+        if (hex == HEX_INVALID) {
+            return SYNTAX_ERROR;
+        }
+        *pDecimal = (*pDecimal << 4) | hex;
+    }
+    return SUCCESS;
+}
+
+uint8_t calculate_checksum(const char *line, uint32_t sizeForLine) {
+    uint8_t checksum = 0;
+    for (uint32_t i = 2; i < sizeForLine - 1; i += 2) {
+        uint32_t convertTwoByte;
+        StrtoHex(&line[i], 2, &convertTwoByte);
+        checksum += convertTwoByte;
+    }
+    return ~checksum & 0xFF;
+}
+
+SrecStatus_t ReadCheckLine(const char *line, Srec *line1) {
+    if (line1 == NULL) {
+        return BYTE_COUNT_ERROR;
+    }
+
+    if (line[0] != 'S') {
+        return SYNTAX_ERROR;
+    }
+
+    uint8_t a2Number = char_to_hex(line[1]);
+    if (a2Number == HEX_INVALID) {
+        return SYNTAX_ERROR;
+    }
+
+    switch (a2Number) {
+        case 1: case 5: case 9:
+            line1->sizeOfAddress = 4;
+            break;
+        case 2: case 6: case 8:
+            line1->sizeOfAddress = 6;
+            break;
+        default:
+            line1->sizeOfAddress = 8;
+    }
+    line1->recordType = a2Number;
+
+    uint32_t byteCount;
+    if (StrtoHex(line + 2, 2, &byteCount) != SUCCESS) {
+        return SYNTAX_ERROR;
+    }
+    line1->ByteCount = byteCount;
+
+    uint8_t lengthAddress = line1->sizeOfAddress;
+    line1->Address = (char *)malloc(lengthAddress + 1);
+    if (line1->Address == NULL) {
+        return BYTE_COUNT_ERROR;
+    }
+    memcpy(line1->Address, &line[4], lengthAddress);
+    line1->Address[lengthAddress] = '\0';
+
+    uint32_t pAddress;
+    if (StrtoHex(line1->Address, lengthAddress, &pAddress) != SUCCESS) {
+        free(line1->Address);
+        return SYNTAX_ERROR;
+    }
+    line1->numberAddress = pAddress;
+
+    uint32_t numberDataByte = byteCount - 1 - (lengthAddress / 2);
+    line1->sizeOfData = numberDataByte * 2;
+    line1->Data = (char *)malloc(line1->sizeOfData + 1);
+    if (line1->Data == NULL) {
+        free(line1->Address);
+        return BYTE_COUNT_ERROR;
+    }
+    memcpy(line1->Data, &line[4 + lengthAddress], line1->sizeOfData);
+    line1->Data[line1->sizeOfData] = '\0';
+
+    for (uint32_t j = 0; j < line1->sizeOfData; j++) {
+        if (char_to_hex(line1->Data[j]) == HEX_INVALID) {
+            free(line1->Address);
+            free(line1->Data);
+            return SYNTAX_ERROR;
+        }
+    }
+
+    uint32_t checkSum;
+    if (StrtoHex(line + 4 + lengthAddress + line1->sizeOfData, 2, &checkSum) != SUCCESS) {
+        free(line1->Address);
+        free(line1->Data);
+        return SYNTAX_ERROR;
+    }
+    line1->checkSum = checkSum;
+
+    uint32_t sizeForLine = byteCount * 2 + 2;
+    char lineForCaculate[sizeForLine + 1];
+    strncpy(lineForCaculate, line, sizeForLine);
+    lineForCaculate[sizeForLine] = '\0';
+    if (calculate_checksum(lineForCaculate, sizeForLine) != line1->checkSum) {
+        free(line1->Address);
+        free(line1->Data);
+        return CHECK_SUM_ERROR;
+    }
+
+    return SREC_TRUE;
+}
+
+Srec* parse_srec(const char *line) {
+    Srec *srec = (Srec *)malloc(sizeof(Srec));
+    if (srec == NULL) {
+        return NULL;
+    }
+
+    if (ReadCheckLine(line, srec) != SREC_TRUE) {
+        free(srec);
+        return NULL;
+    }
+
+    if (srec->recordType != 1 && srec->recordType != 2 && srec->recordType != 3) {
+        free(srec);
+        return NULL;
+    }
+
+    return srec;
+}
+
+void Program_Flash(uint32_t startAddress, char* data, uint32_t length) {
+
+    uint32_t address = startAddress;
+    uint32_t dataWord;
+    uint32_t fullWords = length / 8;
+    uint32_t remainingBytes = length % 8;
+    char buffer[9] = {0};
+
+    for (uint32_t i = 0; i < fullWords * 8; i += 8) {
+        if (StrtoHex(&data[i], 8, &dataWord) == SUCCESS) {
+            Program_LongWord_Command(address, dataWord);
+            address += 4;
+        }
+    }
+
+    if (remainingBytes > 0) {
+        strncpy(buffer, &data[fullWords * 8], remainingBytes);
+        memset(buffer + remainingBytes, '0', 8 - remainingBytes);
+        buffer[8] = '\0';
+
+        if (StrtoHex(buffer, 8, &dataWord) == SUCCESS) {
+            Program_LongWord_Command(address, dataWord);
+        }
+    }
+}
+
+void Process_SREC(char *line) {
+    Srec *srec = parse_srec(line);
+    if (srec != NULL) {
+        Program_Flash(srec->numberAddress, srec->Data, srec->sizeOfData);
+        free(srec->Address);
+        free(srec->Data);
+        free(srec);
+    }
+}
+
+void UART0_IRQHandler(void) {
+    if (UART0->S1 & UART0_S1_RDRF_MASK) {
+        uint8_t receivedChar = UART0->D;
+        rxBuffer[rxIndex++] = receivedChar;
+        if (rxIndex >= BUFFER_SIZE || receivedChar == '\n') { // phải sửa đoạn này phần End Of File nữa
+            newData = true;
+        }
+    }
+}
+
+int main(void) {
+    UART_Initialize();
+
+    Erase_Flash_Sector(0x020400);
+    Erase_Flash_Sector(0x020800);
+    Erase_Flash_Sector(0x020C00);
+    Erase_Flash_Sector(0x021000);
+
+        uint8_t Message[] = "Ready to receive data...\r\n";
+        UART_TransmitString(Message, sizeof(Message) - 1);
+
+        while (1) {
+            if (newData) {
+                newData = false;
+                // Add received data to queue
+                Queue_Enqueue(&srecQueue, (char *)rxBuffer, rxIndex);
+                rxIndex = 0; // Reset buffer index
+            }
+
+            QueueItem item;
+            if (Queue_Dequeue(&srecQueue, &item)) {
+                Process_SREC(item.data);
+            }
+        }
+
+       return 0;
+
+//    UART_Initialize();
+//
+//       Erase_Flash_Sector(0x020400);
+//       const char *srecData[] = {
+//           "S21402040000600020D50402004305020045050200F4",
+//   		"S21402041000000000000000000000000000000000D5",
+//   		"S2140204200000000000000000000000004705020077",
+//   		NULL // Đánh dấu kết thúc
+//       };
+//
+//       // Đưa các dòng SREC vào hàng đợi
+//       for (const char **line = srecData; *line != NULL; line++) {
+//           Queue_Enqueue(&srecQueue, (char *)*line, strlen(*line));
+//       }
+//
+//       // Main loop để dequeue và xử lý các dòng SREC
+//       while (1) {
+//           QueueItem item;
+//           if (Queue_Dequeue(&srecQueue, &item)) {
+//               Process_SREC(item.data);
+//           } else {
+//               break; // Thoát vòng lặp khi hàng đợi trống
+//           }
+//       }
+//
+//       return 0;
+    }
